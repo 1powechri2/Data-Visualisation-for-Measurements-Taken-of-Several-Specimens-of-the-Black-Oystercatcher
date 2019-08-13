71659270da014c9c07112c7e9f5db51fd52421fc
Chris Powell <chris.ian.powell@gmail.com>
2019-08-13 14:27:33 -0600

    Adds template for rendering data index

--- data_playground/views.py
+++ data_playground/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlackOystercatcher
+from django.template import loader
 
 def index(request):
-    return HttpResponse(
-    "<h1>LET'S GET SOME DATA</h1>"
-    )
+    boyds = BlackOystercatcher.objects.all()
+    template = loader.get_template('data_playground/index.html')
+    context = { 'boyds' : boyds }
+    return HttpResponse(template.render(context, request))
