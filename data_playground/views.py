d0b8ea22ae5b7e4f046228b37d73d05cba7bb2ff
Chris Powell <chris.ian.powell@gmail.com>
2019-08-13 15:43:33 -0600

    Added django.pandas to project

--- data_playground/views.py
+++ data_playground/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import BlackOystercatcher
 from django.template import loader
+from django_pandas.io import read_frame
 
 def index(request):
     boyds = BlackOystercatcher.objects.all()
+    df = read_frame(boyds)
     template = loader.get_template('data_playground/index.html')
-    context = { 'boyds' : boyds }
+    context = { 'boyds' : boyds,
+              'df' : df }
     return HttpResponse(template.render(context, request))
