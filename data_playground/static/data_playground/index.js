f8e59cd66c727736b63e66331f0d9737a3e677dc
Chris Powell <chris.ian.powell@gmail.com>
2019-08-28 18:09:19 -0600

    Adds python json library to pass data to react

--- data_playground/static/data_playground/index.js
+++ data_playground/static/data_playground/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var dude = "dude"
-function Welcome(name) {
-  return <h1>Hello, {name}</h1>;
+function stuff() {
+  return <h1>{window.stuff}</h1>;
 }
 
-const element = Welcome(dude);
+const element = stuff();
+
 ReactDOM.render(
   element,
   document.getElementById('react')
--- data_playground/views.py
+++ data_playground/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 from .models import BlackOystercatcher
 from django.template import loader
 from django_pandas.io import read_frame
+import json
 
 def index(request):
     boyds = BlackOystercatcher.objects.all()
     df = read_frame(boyds)
+    ids = df['bird_id']
     data = df.head(0)
     template = loader.get_template('data_playground/index.html')
     context = {
-              'boyds' : boyds,
+              'props' : json.dumps(ids.tolist()),
               'df' : df,
-              'data' : data
+              'data' : data,
+              'ids' : ids
               }
     return HttpResponse(template.render(context, request))
